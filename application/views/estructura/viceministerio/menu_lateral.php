acd0741dadfeee6049623c76f93950b307eafa3a
PaolaGema <gemajancopaola@gmial.com>
2023-12-10 17:21:21 -0400

    mi proyecto

---
+++ application/views/estructura/viceministerio/menu_lateral.php
@@ -0,0 +1,48 @@
+<div class="aside aside-left aside-fixed d-flex flex-column flex-row-auto" id="kt_aside">
+					<!--begin::Brand-->
+					<div class="brand flex-column-auto" id="kt_brand">
+						<!--begin::Logo-->
+						<a href="index.html" class="brand-logo">
+							<img alt="Logo" class="w-65px" src="<?php echo base_url(); ?>recursos_metronics/assets/media/logos/logo-letter-13.png" />
+						</a>
+						<!--end::Logo-->
+					</div>
+					<!--end::Brand-->
+					<!--begin::Aside Menu-->
+					<div class="aside-menu-wrapper flex-column-fluid" id="kt_aside_menu_wrapper">
+						<!--begin::Menu Container-->
+						<div id="kt_aside_menu" class="aside-menu my-4" data-menu-vertical="1" data-menu-scroll="1" data-menu-dropdown-timeout="500">
+							<!--begin::Menu Nav-->
+							<ul class="menu-nav">
+								<li class="menu-item menu-item-active" aria-haspopup="true">
+									<a href="1" class="menu-link">
+										<i class="menu-icon flaticon2-architecture-and-city"></i>
+										<span class="menu-text">Export</span>
+									</a>
+								</li>
+								<li class="menu-item" aria-haspopup="true">
+									<a href="2" class="menu-link">
+										<i class="menu-icon flaticon2-console"></i>
+										<span class="menu-text">Usuarios</span>
+									</a>
+								</li>
+								<li class="menu-item" aria-haspopup="true">
+									<a href="4" class="menu-link">
+										<i class="menu-icon flaticon2-console"></i>
+										<span class="menu-text">Reportes</span>
+									</a>
+								</li>
+								
+								<li class="menu-item" aria-haspopup="true">
+									<a href="#" class="menu-link">
+										<i class="menu-icon flaticon2-graph-1"></i>
+										<span class="menu-text">Estadísticas</span>
+									</a>
+								</li>
+							</ul>
+							<!--end::Menu Nav-->
+						</div>
+						<!--end::Menu Container-->
+					</div>
+					<!--end::Aside Menu-->
+				</div>
